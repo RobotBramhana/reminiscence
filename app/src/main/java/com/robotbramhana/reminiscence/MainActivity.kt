1fba636fd94df134d14aa382cb4684ace1598267
Sudhanva S <40983389+RobotBramhana@users.noreply.github.com>
2026-02-15 18:41:04 +0530

    Update MainActivity.kt

--- app/src/main/java/com/robotbramhana/reminiscence/MainActivity.kt
+++ app/src/main/java/com/robotbramhana/reminiscence/MainActivity.kt
@@ -3,20 +3,32 @@ package com.robotbramhana.reminiscence
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val url = "http://tiny.local:8080" // <-- your Pi server URL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Full-screen kiosk mode
         window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
             View.SYSTEM_UI_FLAG_FULLSCREEN or
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
-        if (!isOnline()) {
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient() // open links inside the WebView
+        webView.settings.javaScriptEnabled = true
+
+        if (isOnline()) {
+            webView.loadUrl(url)
+        } else {
             // TODO: show offline UI
         }
     }
