60b5398757ced7fcc3abb5c748d110d34901c1e4
Sudhanva S <pioneer.sudhu@gmail.com>
2026-02-15 18:03:18 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,33 +4,30 @@ plugins {
 }
 
 android {
-
-    namespace = "com.example.reminiscence"
+    namespace = "com.sudhanva.reminiscence"
     compileSdk = 34
 
     defaultConfig {
-
-        applicationId = "com.example.reminiscence"
-        minSdk = 24
+        applicationId = "com.sudhanva.reminiscence"
+        minSdk = 23
         targetSdk = 34
-
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
-
         release {
             isMinifyEnabled = false
         }
     }
 
     buildFeatures {
-        buildConfig = true
+        viewBinding = true
     }
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 }
